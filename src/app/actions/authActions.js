d67a2c035124bc0d6c48f2f1ac0299f7e6586877
Andrew <andrewriznykua93@gmail.com>
2020-04-29 18:33:32 -0500

    update password action created

--- src/app/actions/authActions.js
+++ src/app/actions/authActions.js
@@ -1,5 +1,6 @@
 import { AUTHENTICATE_USER, LOGOUT } from "../constants/auth";
-import { signUpUser, signInUser, signOutUser } from "../services/auth.service";
+import { SET_ERROR, DELETE_ERROR } from "../constants/error";
+import { signUpUser, signInUser, signOutUser, updateMyPassword } from "../services/auth.service";
 
 export const createUser = userData => async dispatch => {
   try {
@@ -9,8 +10,19 @@ export const createUser = userData => async dispatch => {
       type: AUTHENTICATE_USER,
       payload: user
     })
+
+    dispatch({
+      type: DELETE_ERROR,
+      payload: null
+    })
+
   } catch (err) {
-    console.log(err)
+    if (err.response.data.message){
+      dispatch({
+        type: SET_ERROR,
+        payload: {registerError: err.response.data.message}
+      })
+    }
   }
 }
 
@@ -23,8 +35,18 @@ export const loginUser = userData => async dispatch => {
       type: AUTHENTICATE_USER,
       payload: user
     })
+
+    dispatch({
+      type: DELETE_ERROR,
+      payload: null
+    })
   } catch (err) {
-    console.log(err);
+    if (err.response.data.message){
+      dispatch({
+        type: SET_ERROR,
+        payload: {loginError: err.response.data.message}
+      })
+    }
   }
 }
 
@@ -35,8 +57,36 @@ export const logout = () => async dispatch => {
       type: LOGOUT,
       payload: null
     })
-  } catch (error) {
-    console.log(error)
+  } catch (err) {
+    if (err.response.data.message){
+      dispatch({
+        type: SET_ERROR,
+        payload: {logoutError: err.response.data.message}
+      })
+    }
+  }
+}
+
+export const updatePassword = userData => async dispatch => {
+  try {
+    const res = await updateMyPassword(userData);
+    const user = res.data.data;
+    dispatch({
+      type: AUTHENTICATE_USER,
+      payload: user
+    })
+
+    dispatch({
+      type: DELETE_ERROR,
+      payload: null
+    })
+  } catch (err) {
+    if (err.response.data.message){
+      dispatch({
+        type: SET_ERROR,
+        payload: {updatePasswordError: err.response.data.message}
+      })
+    }
   }
 }
 
