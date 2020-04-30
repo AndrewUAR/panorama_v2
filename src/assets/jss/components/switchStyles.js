aa800f9971835f5e6b7d7641f78d3662e79721a8
Andrew <andrewriznykua93@gmail.com>
2020-04-30 13:57:03 -0500

    styles for switch added

---
+++ src/assets/jss/components/switchStyles.js
@@ -0,0 +1,34 @@
+import { whiteColor, dangerColor, defaultFont, successColor } from '../panoramaStyles';
+
+const switchStyles = theme => ({
+  container: {
+    margin: '0',
+    padding: "0.3rem 1rem",
+    color: whiteColor,
+    "& > :last-child": {
+      ...defaultFont,
+      fontSize: "1rem",
+      marginRight: "auto",
+    }
+  },
+  switchBase: {
+    color: dangerColor,
+    '&$checked': {
+      color: "#38ef7d"
+    },
+    '&$checked + $track': {
+      background: "linear-gradient(to right, #aaffa9, #11ffbd)"
+    }
+  },
+  checked: {
+    color: successColor
+  },
+  track: {
+    borderRadius: "26 / 2",
+    border: `1px solid`,
+    background: "linear-gradient(to right, #ef3b36, #ffffff)",
+    opacity: "1"
+  }
+});
+
+export default switchStyles;
