9279cfa0a8814435b2b10020eeef7b705044f9d4
Andrew <andrewriznykua93@gmail.com>
2020-05-04 13:34:59 -0500

    LoadingComponent created

--- src/app/layout/LoadingComponent.jsx
+++ src/app/layout/LoadingComponent.jsx
@@ -1,34 +1,24 @@
-import React from 'react';
-import Backdrop from '@material-ui/core/Backdrop';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import {ScaleLoader} from "react-spinners";
+import styles from "../../assets/jss/components/loadingComponentStyle";
+import { makeStyles } from "@material-ui/core";
 
-const useStyles = makeStyles((theme) => ({
-  backdrop: {
-    zIndex: theme.zIndex.drawer + 1,
-    color: '#fff',
-  },
-}));
+const useStyles = makeStyles(styles);
 
-export default function SimpleBackdrop() {
+const LoadingComponent = props => {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
-  const handleClose = () => {
-    setOpen(false);
-  };
-  const handleToggle = () => {
-    setOpen(!open);
-  };
-
+  
   return (
-    <div>
-      <Button variant="outlined" color="primary" onClick={handleToggle}>
-        Show backdrop
-      </Button>
-      <Backdrop className={classes.backdrop} open={true} onClick={handleClose}>
-        <CircularProgress color="inherit" />
-      </Backdrop>
+    <div className={classes.container}>
+      <ScaleLoader
+        color={"#00c6ff"}
+        loading={true}
+        height={50}
+        width={6}
+        margin={3}
+      />
     </div>
-  );
+  )
 }
+
+export default LoadingComponent;
